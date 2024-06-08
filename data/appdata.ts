9a6fa117bfa96299bf4d315a5a3a278eebfa4911
Supanut1911 <ynotmela@gmail.com>
2024-06-08 15:02:34 +0700

    display grid with mock data

---
+++ data/appdata.ts
@@ -0,0 +1,44 @@
+import { AppDataType } from "@/types/app_data_type"
+
+const appDatas: AppDataType[]= [
+    {
+        id: 1,
+        name: 'Angular',
+        description: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript. Angular is written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your apps.',
+        imageUrl: 'https://angular.io/assets/images/logos/angular/angular.svg'
+    },
+    {
+        id: 2,
+        name: 'React',
+        description: 'React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+    },
+    {
+        id: 3,
+        name: 'Vue',
+        description: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo_2.svg'
+    },
+    {
+        id: 4,
+        name: 'Vue',
+        description: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo_2.svg'
+    },
+    {
+        id: 5,
+        name: 'Vue',
+        description: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo_2.svg'
+    },
+    {
+        id: 6,
+        name: 'Vue',
+        description: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo_2.svg'
+    },
+    
+
+]
+
+export  default appDatas
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,41 +8,41 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Inter } from "next/font/google";
+import appdata from '../data/appdata'
+import { AppDataType } from "@/types/app_data_type";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const cardArr = [1,2,3,4,5,6,7,8]
-
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex ">
-        EOL Checklist 
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex ">
+        EOL Checklist  ⌛️ 🛠️
       </div>
 
       <div className="grid grid-cols-4 gap-4">
-        {cardArr.map((card, idx) => {
+        {appdata.map((card: AppDataType, idx) => {
           return (
             <Card className="w-[350px]" key={idx}>
               <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle>{card.name}</CardTitle>
               </CardHeader>
               <CardContent>
-                <p>Card Content</p>
+                <Image src={card.imageUrl} alt={card.name} width={200} height={200} />
               </CardContent>
               <CardFooter>
-                <Button>Button</Button>
+                <Button>View more detail</Button>
               </CardFooter>
             </Card>
           )
         })}
       </div>
 
-    <div>test</div>
+    <div></div>
       
     </main>
   );

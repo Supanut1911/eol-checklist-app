a64408b81985990e6ba2713102f2b019ed92836c
Supanut1911 <ynotmela@gmail.com>
2024-06-08 14:53:10 +0700

    add card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,49 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const cardArr = [1,2,3,4,5,6,7,8]
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        hi
-      </div>
-      {/* <div>
-        <Button>Click</Button>
-      </div> */}
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        test
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex ">
+        EOL Checklist 
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        space
+      <div className="grid grid-cols-4 gap-4">
+        {cardArr.map((card, idx) => {
+          return (
+            <Card className="w-[350px]" key={idx}>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <Button>Button</Button>
+              </CardFooter>
+            </Card>
+          )
+        })}
       </div>
+
+    <div>test</div>
+      
     </main>
   );
 }
